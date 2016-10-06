8c122c85110deb93f29e19702c276dac6df6fa47
marqh <markh@metarelate.net>
2016-10-06 07:30:42 +0000

    testing

--- lib/bald/tests/unit/test_HttpCache.py
+++ lib/bald/tests/unit/test_HttpCache.py
@@ -4,16 +4,19 @@ import h5py
 import numpy as np
 
 from bald.tests import BaldTestCase
-from bald import validation
+import bald
 
-class Test(unittest.TestCase):
+class TestHttpCache(unittest.TestCase):
+    def setUp(self):
+        self.cache = bald.HttpCache()
+
     def test_check_uri_200(self):
         auri = 'http://binary-array-ld.net/experimental'
-        self.assertTrue(validation.check_uri(auri))
+        self.assertTrue(self.cache.check_uri(auri))
 
     def test_check_uri_404(self):
         notauri = 'http://binary-array-ld.net/experimentalish'
-        self.assertFalse(validation.check_uri(notauri))
+        self.assertFalse(self.cache.check_uri(notauri))
 
 
 if __name__ == '__main__':
