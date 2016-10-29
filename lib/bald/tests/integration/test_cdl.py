11c313693c7548cbd349cccf82334ae2928ba1db
marqh <markh@metarelate.net>
2016-10-29 07:20:11 +0000

    test_cdl

---
+++ lib/bald/tests/integration/test_cdl.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+import unittest
+
+import netCDF4
+import numpy as np
+
+import bald
+from bald.tests import BaldTestCase
+
+class Test(BaldTestCase):
+    def setUp(self):
+        self.cdl_path = os.path.join(os.path.dirname(__file__), 'CDL')
+        print(self.cdl_path)
+        
+    def test_valid_uri(self):
+        with self.temp_filename('.nc') as tfile:
+            cdl_file = os.path.join(self.cdl_path, 'array_reference.cdl')
+            subprocess.check_call(['ncgen', '-o', tfile, cdl_file])
+            validation = bald.validate_netcdf(tfile)
+            self.assertTrue(validation.is_valid())
