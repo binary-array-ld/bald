52736fb7940b9a18e2d99f4c0a15c349782d7ae5
Jonathan Yu <jonathan.yu@csiro.au>
2017-02-14 18:12:04 +1100

    Adding collection definition

--- utils/bootstrapping/generate_registers.py
+++ utils/bootstrapping/generate_registers.py
@@ -1,28 +1,53 @@
 import json
-from pprint import pprint
-import rdflib
-from rdflib import Graph, Namespace, URIRef, BNode, Literal
-from rdflib.namespace import RDF, DC, RDFS
-
+from rdflib import Graph, Namespace, URIRef, Literal
+from rdflib.namespace import RDF, DC, RDFS,SKOS
 
 with open('aliases.json') as data_file:
     data = json.load(data_file)
 
-
 g = Graph()
 
 NCLD_CLASS_str = "http://binary-array-ld.net/def/nc/classes/"
-NCLD_VOCAB_str = "http://binary-array-ld.net/def/nc/vocab/"
+ACDD_NAMES_str = "http://binary-array-ld.net/def/acdd-names/"
+CF_NAMES_str = "http://binary-array-ld.net/def/cf-names/"
+NC_NAMES_str = "http://binary-array-ld.net/def/nc-names/"
+CONVENTIONS_str = "http://binary-array-ld.net/def/conventions/"
+UNKNOWN_str = "http://binary-array-ld.net/def/unknown-convention/"
 NCLD_CLASS = Namespace(NCLD_CLASS_str) #Namespace TBC
-NCLD_VOCAB = Namespace(NCLD_VOCAB_str) #Namespace TBC
+ACDD_NAMES = Namespace(ACDD_NAMES_str) #Namespace TBC
+NC_NAMES = Namespace(NC_NAMES_str) #Namespace TBC
+CF_NAMES = Namespace(CF_NAMES_str) #Namespace TBC
 
 g.bind("dc", DC)
 g.bind("nc-class", NCLD_CLASS)
-g.bind("nc-vocab", NCLD_VOCAB)
+g.bind("cf-names", CF_NAMES)
+g.bind("nc-names", NC_NAMES)
+g.bind("skos", SKOS)
+g.bind("acdd-names", ACDD_NAMES)
+
+acdd_conv = URIRef(CONVENTIONS_str + "ACDD")
+cf_conv = URIRef(CONVENTIONS_str + "CF")
+nc_conv = URIRef(CONVENTIONS_str + "NetCDF")
+
+
+g.add( (acdd_conv, RDF.type, SKOS.Collection))
+g.add( (cf_conv, RDF.type, SKOS.Collection))
+g.add( (nc_conv, RDF.type, SKOS.Collection))
+
 
 for key in data['names']:
     link = data['names'][key]
-    subj = URIRef(NCLD_VOCAB_str + key)
+    if(link.startswith("http://wiki.esipfed.org")):
+        subj = URIRef(ACDD_NAMES_str + key)
+        g.add((acdd_conv, SKOS.member, subj))
+    elif(link.startswith("http://www.unidata.ucar.edu")):
+        subj = URIRef(NC_NAMES_str + key)
+        g.add((nc_conv, SKOS.member, subj))
+    elif (link.startswith("http://cfconventions.org/cf-conventions/v1.6.0/")):
+        subj = URIRef(CF_NAMES_str + key)
+        g.add( (cf_conv, SKOS.member, subj))
+    else:
+        subj = URIRef(UNKNOWN_str + key)
     type = URIRef(NCLD_CLASS.Attribute)
     g.add( (subj, DC.identifier, Literal(key)) )
     g.add((subj, RDF.type, type))
