e61397d706366792a125f21bba3eab859499d1f1
Sean Arms <sarms@ucar.edu>
2017-10-24 16:28:38 -0600

    Fix binary-array-ld/bald#64
    Allow netCDF 4 files with the extension .nc4 to be read by bald

--- lib/bald/__init__.py
+++ lib/bald/__init__.py
@@ -528,7 +528,7 @@ def load(afilepath):
 def load(afilepath):
     if afilepath.endswith('.hdf'):
         loader = h5py.File
-    elif afilepath.endswith('.nc'):
+    elif afilepath.endswith('.nc') or afilepath.endswith('nc4'):
         loader = netCDF4.Dataset
     else:
         raise ValueError('filepath suffix not supported')
