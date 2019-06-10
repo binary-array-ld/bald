211776503d2c11527308311a344547594624e724
markh <markh@metarelate.net>
2019-06-10 20:01:57 +0000

    conformA

---
+++ lib/bald/tests/integration/test_ogc_conformance.py
@@ -0,0 +1,79 @@
+import os
+import subprocess
+import unittest
+
+import netCDF4
+import numpy as np
+
+import bald
+from bald.tests import BaldTestCase
+
+class Test(BaldTestCase):
+    def setUp(self):
+        self.cdl_path = os.path.join(os.path.dirname(__file__), 'CDL')
+        self.ttl_path = os.path.join(os.path.dirname(__file__), 'TTL')
+        self.maxDiff = None
+        
+    def test_conformance_a(self):
+        with self.temp_filename('.nc') as tfile:
+            cdlname = 'ogcClassA.cdl'
+            cdl_file = os.path.join(self.cdl_path, cdlname)
+            subprocess.check_call(['ncgen', '-o', tfile, cdl_file])
+            cdl_file_uri = 'http://secret.binary-array-ld.net/identity.nc'
+            root_container = bald.load_netcdf(tfile, baseuri=cdl_file_uri, cache=self.acache)
+            ttl = root_container.rdfgraph().serialize(format='n3').decode("utf-8")
+            if os.environ.get('bald_update_results') is not None:
+                with open(os.path.join(self.ttl_path, 'ogcClassA.ttl'), 'w') as sf:
+                    sf.write(ttl)
+            with open(os.path.join(self.ttl_path, 'ogcClassA.ttl'), 'r') as sf:
+                expected_ttl = sf.read()
+            self.assertEqual(expected_ttl, ttl)
+
+    def test_conformance_b(self):
+        with self.temp_filename('.nc') as tfile:
+            cdlname = 'ogcClassB.cdl'
+            cdl_file = os.path.join(self.cdl_path, cdlname)
+            subprocess.check_call(['ncgen', '-o', tfile, cdl_file])
+            cdl_file_uri = 'http://secret.binary-array-ld.net/prefix.nc'
+            root_container = bald.load_netcdf(tfile, baseuri=cdl_file_uri, cache=self.acache)
+            ttl = root_container.rdfgraph().serialize(format='n3').decode("utf-8")
+            if os.environ.get('bald_update_results') is not None:
+                with open(os.path.join(self.ttl_path, 'ogcClassB.ttl'), 'w') as sf:
+                    sf.write(ttl)
+            with open(os.path.join(self.ttl_path, 'ogcClassB.ttl'), 'r') as sf:
+                expected_ttl = sf.read()
+            self.assertEqual(expected_ttl, ttl)
+
+    def test_conformance_c(self):
+        with self.temp_filename('.nc') as tfile:
+            cdlname = 'ogcClassC.cdl'
+            cdl_file = os.path.join(self.cdl_path, cdlname)
+            subprocess.check_call(['ncgen', '-o', tfile, cdl_file])
+            cdl_file_uri = 'http://secret.binary-array-ld.net/alias.nc'
+            alias_dict = {'NetCDF': 'http://def.scitools.org.uk/NetCDF'}
+            root_container = bald.load_netcdf(tfile, baseuri=cdl_file_uri,
+                                              alias_dict=alias_dict, cache=self.acache)
+            ttl = root_container.rdfgraph().serialize(format='n3').decode("utf-8")
+            if os.environ.get('bald_update_results') is not None:
+                with open(os.path.join(self.ttl_path, 'ogcClassC.ttl'), 'w') as sf:
+                    sf.write(ttl)
+            with open(os.path.join(self.ttl_path, 'ogcClassC.ttl'), 'r') as sf:
+                expected_ttl = sf.read()
+            self.assertEqual(expected_ttl, ttl)
+
+    def test_conformance_d(self):
+        with self.temp_filename('.nc') as tfile:
+            cdlname = 'ogcClassD.cdl'
+            cdl_file = os.path.join(self.cdl_path, cdlname)
+            subprocess.check_call(['ncgen', '-o', tfile, cdl_file])
+            cdl_file_uri = 'http://secret.binary-array-ld.net/attributes.nc'
+            alias_dict = {'NetCDF': 'http://def.scitools.org.uk/NetCDF'}
+            root_container = bald.load_netcdf(tfile, baseuri=cdl_file_uri,
+                                              alias_dict=alias_dict, cache=self.acache)
+            ttl = root_container.rdfgraph().serialize(format='n3').decode("utf-8")
+            if os.environ.get('bald_update_results') is not None:
+                with open(os.path.join(self.ttl_path, 'ogcClassD.ttl'), 'w') as sf:
+                    sf.write(ttl)
+            with open(os.path.join(self.ttl_path, 'ogcClassD.ttl'), 'r') as sf:
+                expected_ttl = sf.read()
+            self.assertEqual(expected_ttl, ttl)
