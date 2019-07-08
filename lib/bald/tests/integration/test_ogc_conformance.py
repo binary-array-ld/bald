47b73181f161e16ac86e20840fa9e19f06221950
marqh <markh@metarelate.net>
2019-07-08 20:34:52 +0000

    conformance class d

--- lib/bald/tests/integration/test_ogc_conformance.py
+++ lib/bald/tests/integration/test_ogc_conformance.py
@@ -14,7 +14,7 @@ class Test(BaldTestCase):
         self.ttl_path = os.path.join(os.path.dirname(__file__), 'TTL')
         self.maxDiff = None
         
-    def test_conformance_A(self):
+    def test_conformance_a(self):
         with self.temp_filename('.nc') as tfile:
             cdlname = 'ogcClassA.cdl'
             cdl_file = os.path.join(self.cdl_path, cdlname)
@@ -58,5 +58,22 @@ class Test(BaldTestCase):
                 with open(os.path.join(self.ttl_path, 'ogcClassC.ttl'), 'w') as sf:
                     sf.write(ttl)
             with open(os.path.join(self.ttl_path, 'ogcClassC.ttl'), 'r') as sf:
+                expected_ttl = sf.read()
+            self.assertEqual(expected_ttl, ttl)
+
+    def test_conformance_d(self):
+        with self.temp_filename('.nc') as tfile:
+            cdlname = 'ogcClassD.cdl'
+            cdl_file = os.path.join(self.cdl_path, cdlname)
+            subprocess.check_call(['ncgen', '-o', tfile, cdl_file])
+            cdl_file_uri = 'http://secret.binary-array-ld.net/attributes.nc'
+            alias_dict = {'NetCDF': 'http://def.scitools.org.uk/NetCDF'}
+            root_container = bald.load_netcdf(tfile, baseuri=cdl_file_uri,
+                                              alias_dict=alias_dict, cache=self.acache)
+            ttl = root_container.rdfgraph().serialize(format='n3').decode("utf-8")
+            if os.environ.get('bald_update_results') is not None:
+                with open(os.path.join(self.ttl_path, 'ogcClassD.ttl'), 'w') as sf:
+                    sf.write(ttl)
+            with open(os.path.join(self.ttl_path, 'ogcClassD.ttl'), 'r') as sf:
                 expected_ttl = sf.read()
             self.assertEqual(expected_ttl, ttl)
