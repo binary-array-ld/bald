b9ebd5238c7211846df54fd5b479662eda95387a
Jonathan Yu <jonathan.yu@csiro.au>
2022-09-30 09:54:48 +1000

    modifying affected tests

--- lib/bald/tests/integration/test_netcdf.py
+++ lib/bald/tests/integration/test_netcdf.py
@@ -51,8 +51,8 @@ class Test(BaldTestCase):
             validation = bald.validate_netcdf(tfile, cache=self.acache,
                                               uris_resolve=True)
             exns = validation.exceptions()
-            expected = ['https://www.opengis.net/def/binary-array-ld/turtle is not resolving as a resource (404).',
-                        'https://www.opengis.net/def/binary-array-ld/walnut is not resolving as a resource (404).']
+            expected = ['https://www.opengis.net/def/binary-array-ld/ is not resolving as a resource (404).', 'https://www.opengis.net/def/binary-array-ld/turtle is not resolving as a resource (404).', 'https://www.opengis.net/def/binary-array-ld/walnut is not resolving as a resource (404).', 'https://www.opengis.net/def/binary-array-ld/ is not resolving as a resource (404).', 'https://www.opengis.net/def/binary-array-ld/ is not resolving as a resource (404).']
+
             self.assertTrue((not validation.is_valid()) and exns == expected,
                              msg='{}  != {}'.format(exns, expected))
 
